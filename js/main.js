a039a8fe7df604ca051f61c942804d3bb42536d7
nhk9710 <hknoh@wkit.co.kr>
2022-03-25 11:14:48 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function (){
+    $('#fullpage').fullpage({
+        fingersonly: false,
+        scrollingSpeed: 900,
+        anchors:['firstPage','secondPage','thirdPage','lastPage'],
+        navigation: true,
+        navigationTooltips: ['첫페이지', '두번째페이지', '세번째페이지', '네번째페이지'],
+        navigationPosition: 'right',
+        showActiveTooltip: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        keyboardScrolling: true,
+        animateAnchor: true,
+        controllArrows: true,
+        menu:'.myNav',
+        sectionsColor : ['skyblue', 'pink', 'white', '#F4F4F4'],
+        fixedElements: 'footer',
+        fadingEffect: true,
+    });
+})
