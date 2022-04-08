f75448d34e80595ca1c639386d4fac5f7f0bee99
nhk9710 <hknoh@wkit.co.kr>
2022-04-08 10:01:53 +0900

    [add] main 4번째 수술

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function (){
     $('#fullpage').fullpage({
         scrollingSpeed: 700,
         anchors:['firstPage','secondPage','lastPage'],
-        navigation: true,
+        navigation: false,
         navigationTooltips: ['첫페이지', '두번째페이지', '세번째페이지'],
         navigationPosition: 'right',
         showActiveTooltip: true,
@@ -12,9 +12,12 @@ $(function (){
         animateAnchor: true,
         controllArrows: true,
         menu:'.myNav',
-        sectionsColor : ['white', 'white', 'white', 'grey'],
+        sectionsColor : ['white', 'white', 'white', 'white'],
         fixedElements: 'footer',
         fadingEffect: true,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        arrows: true
     });
 
     $('#login-page').fullpage({
